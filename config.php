a8755c99011983ac00bda8f216f705b3a46f6cd0
Rafael <gigijanjao@gmail.com>
2015-12-29 23:58:12 -0300

    Adicionando o config.php para configuração do Banco de Dados.

---
+++ config.php
@@ -0,0 +1,9 @@
+<?php
+
+$param = array(
+	'database' => '', // Defina qual o Banco de Sados a ser utilizado
+ 	'host' => '', // Defina o host do Banco de Dados
+ 	'dbaseName' => '', // Defina o nome da Base de dados que será utilizada
+ 	'usuario' => '', // Defina o usuário do banco de dados do seu sistema
+ 	'password' => '' // Defina a Senha do banco de dados do seu sistema
+  	);
